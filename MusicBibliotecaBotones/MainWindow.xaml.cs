b8856a5149416a1065e9706e1ba899531997cf93
AngelMenenedezDAM1 <150004040+AngelMenenedezDAM1@users.noreply.github.com>
2025-02-18 22:34:44 +0100

    Agregar archivos de proyecto.

---
+++ MusicBibliotecaBotones/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicBibliotecaBotones
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BotonExplorador_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
